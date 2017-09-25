e3dea0fa26a2c3e7aef212e6bdd9f7423de34e81
Kyle Ebersole and Rakhee Gandhi <rakhee@>
2017-09-25 10:06:30 -0700

    Add script to collect user input and display on click.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // business logic
-
 function Place(location, landmarks, timeOfYear, notes) {
   this.location = location;
   this.landmarks = landmarks;
@@ -12,13 +11,28 @@ $(document).ready(function() {
   $("form#new-place").submit(function(event){
     event.preventDefault();
 
-var inputtedLocation = $("input#new-location").val();
-var inputtedLandmarks = $("input#new-landmarks").val();
-var inputtedTimeOfYear = $("input#new-timeOfYear").val();
-var inputtedNotes = $("input#new-notes").val();
-var newPlace = new Place(inputtedLocation, inputtedLandmarks, inputtedTimeOfYear, inputtedNotes);
+    var inputtedLocation = $("input#new-location").val();
+    var inputtedLandmarks = $("input#new-landmarks").val();
+    var inputtedTimeOfYear = $("input#new-timeOfYear").val();
+    var inputtedNotes = $("input#new-notes").val();
 
-$("ul#places").append("<li><span class='places'>" + newPlace.location + "</span></li>");
+    var newPlace = new Place(inputtedLocation, inputtedLandmarks, inputtedTimeOfYear, inputtedNotes);
+
+    $("ul#places").append("<li><span class='place'>" + newPlace.location + "</span></li>");
+
+    $('.place').last().click(function(){
+      $('#show-places').show();
+      $('#show-places h2').text(newPlace.location);
+      $('.location').text(newPlace.location);
+      $('.landmarks').text(newPlace.landmarks);
+      $('.timeOfYear').text(newPlace.timeOfYear);
+      $('.notes').text(newPlace.notes);
+    });
+
+    $("input#new-location").val("");
+    $("input#new-landmarks").val("");
+    $("input#new-timeOfYear").val("");
+    $("input#new-notes").val("");
 
   });
 });
